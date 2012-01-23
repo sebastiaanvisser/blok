895848ab6fea3d6130f35384524ac73cb6dea243
Sebastiaan Visser <code@fvisser.nl>
2012-01-23 22:55:36 +0100

    More styling fixes.

--- code/Main.js
+++ code/Main.js
@@ -31,31 +31,36 @@ function start ()
       }
     );
 
-  $(".target").each
+  $(".widget").each
     ( function (_, el)
       {
         var a = new Adjust(el, el, mode);
 
         with (Dsl)
         {
-          var drag = orOrigin
-            ( dragSolver
-              ( function () { return [Geom.parentEl(el)]; }
-              , function () { return Dsl.selector("h1, h2, p, .target", el.parentNode, [el]); }
-              )
-            );
+          var drag =
+            orOrigin
+              ( dragSolver
+                ( function () { return [Geom.parentEl(el)]; }
+                , function () { return Dsl.selector("h1, h2, p, .widget", el.parentNode, [el]); }
+                )
+              );
 
-          var resize = orOrigin
-            ( resizeSolver
-              ( function () { return [Geom.parentEl(el)]; }
-              , function () { return Dsl.selector("h1, h2, p, .target", el.parentNode, [el]); }
-              )
-            );
+          var resize =
+            compose
+              ( orOrigin
+                ( resizeSolver
+                  ( function () { return [Geom.parentEl(el)]; }
+                  , function () { return Dsl.selector("h1, h2, p, .widget", el.parentNode, [el]); }
+                  )
+                )
+              , bounded(24 * 8, 24 * 6)
+              );
 
           a.onDragAlign     = margin(24, strech(1.0, drag));
           a.stopDragAlign   = margin(24, compose(grid(24, 24), drag));
-          a.onResizeAlign   = compose(bounded(24 * 6, 24 * 4), margin(24, strech(1.0, resize)));
-          a.stopResizeAlign = compose(bounded(24 * 6, 24 * 4), margin(24, compose(grid(24, 24), resize)));
+          a.onResizeAlign   = margin(24, strech(1.0, resize));
+          a.stopResizeAlign = margin(24, compose(grid(24, 24), resize));
         }
       }
     );
