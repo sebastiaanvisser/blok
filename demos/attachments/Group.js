4c14b2ef92bd4831d909092a0a2ed637b1b0e3f1
Sebastiaan Visser <code@fvisser.nl>
2012-07-07 11:21:33 +0200

    Made the attachments demo more up to date.

--- demos/attachments/Group.js
+++ demos/attachments/Group.js
@@ -1,33 +1,40 @@
 function Group ()
 {
-  this.containers = [];
-  this.targets    = [];
+  this.containers  = [];
+  this.targets     = [];
+  this.grid        = 1;
+  this.border      = 10;
 }
 
 Group.prototype.addContainer =
   function addContainer (t)
   {
-    var block = new Block(t);
-    this.containers.push(block);
-    return block;
+    this.containers.push(new Block(t));
   };
 
-Group.prototype.addTarget =
-  function addTarget (t)
+Group.prototype.makeTarget =
+  function makeTarget (bg, fg)
   {
-    var block = new Block(t);
-    new Drag(block);
-    new Resize(block);
-    block.initialGeometry(1);
-    this.targets.push(block);
-    return block;
+    var b = $("<div>");
+    var f = $("<div>");
+    $("#bg").append(b);
+    $("#fg").append(f);
+
+    var t = new Block(f, Drag, Resize, Scale);
+    var a = new Block(b, Resize, Scale);
+    t.attachment = a;
+
+    this.targets.push(t);
   };
 
-Group.prototype.touch =
-  function touch ()
+Group.prototype.install =
+  function install ()
   {
-    var containers = this.containers.map (function (block) { return block.target[0]; });
-    var targets    = this.targets.map    (function (block) { return block.target[0]; });
+    var containers  = this.containers.map  (function (block) { return block.target[0];            });
+    var targets     = this.targets.map     (function (block) { return block.target[0];            });
+    var attachments = this.targets.map     (function (block) { return block.attachment.target[0]; });
+
+    var that = this;
 
     this.targets.forEach
       (function (t)
@@ -35,27 +42,68 @@ Group.prototype.touch =
          var cf = function () { return Dsl.fromList(containers, null, []); };
          var of = function () { return Dsl.fromList(targets, null, [t.target[0]]); };
 
-         var dragger       = Dsl.orOrigin(Dsl.bestOf(Dsl.drag(cf, of)));
-         var resize        = Dsl.compose(Dsl.orOrigin(Dsl.resize( cf, of)), Dsl.bounded(12 * 4, 12 * 4));
+         t.drag.moveToTop =
+         t.resize.moveToTop = true;
+
+         var grid          = Dsl.grid(that.grid, that.grid);
+         var bounds        = Dsl.bounded(2 * that.grid, 2 * that.grid);
+         var drag          = Dsl.orOrigin(Dsl.bestOf(Dsl.drag(cf, of)));
+         var resize        = Dsl.compose(Dsl.orOrigin(Dsl.resize(cf, of)), bounds);
 
          t.drag.onDrag     =
-         t.drag.onStop     = Dsl.margin(dragger, 0);
-         t.resize.onResize = 
-         t.resize.onStop   = Dsl.margin(resize, 0);
+         t.drag.onStop     = Dsl.compose(drag, grid);
+         t.resize.onResize =
+         t.resize.onStop   = Dsl.compose(resize, grid);
        });
 
     this.targets.forEach
-      ( function (t, i)
+      (function (t)
+       {
+         t = t.attachment;
+         t.resize.moveToTop = false;
+
+         var cf = function () { return Dsl.fromList(containers, null, []); };
+         var of = function () { return Dsl.fromList(attachments, null, [t.target[0]]); };
+
+         var grid          = Dsl.grid(that.grid, that.grid, that.border, that.border);
+         var bounds        = Dsl.bounded(2 * that.grid, 2 * that.grid);
+         var drag          = Dsl.orOrigin(Dsl.bestOf(Dsl.drag(cf, of)));
+         var resize        = Dsl.compose(Dsl.orOrigin(Dsl.resize(cf, of)), bounds);
+
+         t.resize.onResize =
+         t.resize.onStop   = Dsl.compose(Dsl.margin(resize, -2 * that.border), grid);
+
+       });
+
+    this.targets.forEach
+      ( function (t)
         {
-          t.onRender =
-            [ function (t)
-              {
-                Block.render($($("#bg > div")[i]), Geom.grow(t.geom, 10));
-              }
-            ];
+          var a = t.attachment;
+          t.onAdjust.push(function () { a.adjust(Geom.grow   (t.geom, that.border)); });
+          a.onAdjust.push(function () { t.adjust(Geom.shrink (a.geom, that.border)); });
+
+/*
+          a.onAdjust.push
+           ( function (g)
+             {
+               var ls = Dsl.selector("#bg > div", null, []); //[this.target[0]]);
+               ls.forEach(function (n) { $(n.elem).removeClass("dragover"); });
+               ls = ls.filter(function (n) { return Geom.intersect(g, n); });
+               ls.forEach(function (n) { $(n.elem).addClass("dragover"); });
+             }
+           );
+
+          a.resize.onStopE.push
+           ( function (g)
+             {
+               var ls = Dsl.selector("#bg > div", null, []); //[this.target[0]]);
+               ls.forEach(function (n) { $(n.elem).removeClass("dragover"); });
+             }
+           );
+*/
         }
       );
 
-    this.targets.forEach(function (x) { x.render(); });
+    this.targets.forEach     (function (x) { x.drag.touch(); });
   };
 
--- demos/attachments/Main.js
+++ demos/attachments/Main.js
@@ -1,9 +1,23 @@
 function start ()
 {
-  var group = window.g = new Group;
-  $( "#container" ).each(function (_, t) { group.addContainer (t) });
-  $( ".target"    ).each(function (_, t) { group.addTarget    (t) });
-  g.touch();
+  window.g = new Group;
+
+  g.addContainer($("#container"));
+
+  for (var i = 0; i < 9; i++)
+    g.makeTarget($("#bg"), $("#fg"))
+
+  g.install();
+
+  g.targets[0].adjust({x :  10, y :  10, r :  210, b : 210 });
+  g.targets[1].adjust({x : 210, y :  10, r :  410, b : 210 });
+  g.targets[2].adjust({x : 410, y :  10, r :  610, b : 210 });
+  g.targets[3].adjust({x :  10, y : 210, r :  210, b : 410 });
+  g.targets[4].adjust({x : 810, y : 210, r : 1010, b : 410 });
+  g.targets[5].adjust({x : 410, y : 210, r :  610, b : 410 });
+  g.targets[6].adjust({x :  10, y : 410, r :  210, b : 610 });
+  g.targets[7].adjust({x : 210, y : 410, r :  410, b : 610 });
+  g.targets[8].adjust({x : 410, y : 410, r :  610, b : 610 });
 }
 
 $(document).ready(start);
